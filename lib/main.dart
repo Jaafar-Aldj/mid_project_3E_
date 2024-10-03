54c2ca551c2076cb17bba3dbeab7ae16a089e441
jaafar <anaking1212@gmail.com>
2024-10-03 18:26:44 +0300

    i add cities in a list

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class LeisureTrips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:mid_project_3e_/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LeisureTrips());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
