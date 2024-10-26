c156a90f00fc39f3a0d4d323627bd3858843761d
jaafar <anaking1212@gmail.com>
2024-10-26 19:32:51 +0300

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'models/mysql.dart';
 
 void main() async {
-  await sql();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const LeisureTrips());
 }
 
